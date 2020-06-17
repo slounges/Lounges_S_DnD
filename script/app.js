7bc2d5dc57da2d0afb199971206c9cd9815862ec
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-06-17 19:27:00 -0400

    added image backgrounds

--- script/app.js
+++ script/app.js
@@ -1,12 +1,17 @@
 (() => {
 	// set up the puzzle pieces and boards
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img');
+			gameBoard = document.querySelector(".puzzle-board");
+
+
+
+
 
 	function changeImageSet() {
 		// change all the image elements on the page -> draggable image sources,
+		
 		// and set the drop zone background
-
-		debugger;
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
 	}
 
 	// add event handling here -> how is the user going to use our app?
@@ -14,4 +19,8 @@
 
 	// click on the bottom buttons to change the puzzle image we're working with
 	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+
+	//reasearch call, apply and bind
+	changeImageSet.call(puzzleButtons[0]); //emulates a click on the first bottom button
 })();
