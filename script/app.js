60b7c7d7fe9bb0e304e0ed8377769391a0aed373
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-06-20 19:00:47 -0400

    fixed second bug

--- script/app.js
+++ script/app.js
@@ -5,32 +5,25 @@
 				puzzlePieces = document.querySelectorAll(".puzzle-image"),
 				dropZones = document.querySelectorAll(".drop-zone"),
 				gameBoard = document.querySelector(".puzzle-board"), // one to one relationship -> returns the first matching element
-				zonePieces = document.querySelector('puzzle-pieces');
+				zonePieces = document.querySelector('.puzzle-pieces');
 
 
 
 	let imageNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
 
-	// add event handling here -> how is the user going to use our app?
-	// what triggers do we need?
+	
 	function changeImageSet() {
-		// change all the image elements on the page -> draggable image sources
 		imageNames.forEach((piece, index) => {
 			puzzlePieces[index].src = `images/${piece + this.dataset.bgkey}.jpg`;
 		});
 
-		// and set the drop zone background
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgkey}.jpg)`;
 	}
 
 	function allowDrag(event) {
-		// let the drag happen, and store a reference of the ID of the element we're dragging
 		console.log('started dragging an image: this one - ', event.target.id);
 
 		event.dataTransfer.setData("draggedImg", this.id);
-		// event.dataTransfer.setData("targetTrack", this.dataset.track);
-
-		// set a reference to a data track so i can retrieve it later in the drop
 	}
 
 	function allowDragOver(event) {
@@ -47,24 +40,32 @@
 		//event.preventDefault();
 		console.log('dropped an image');
 
-		// get the dragged element's ID from the data transfer
 		let droppedImage = event.dataTransfer.getData("draggedImg");
 
-		// add that image to whatever drop zone we're dropping on
 		event.target.appendChild(document.querySelector(`#${droppedImage}`));
 	}
 
-
+// resets the puzzle game when the next image is clicked.
+	function resetPuzzlePieces() {
+		for (let i = 0; i < puzzlePieces.length; i++) {
+			zonePieces.appendChild(puzzlePieces[i]);
+		}
+	}
 	
-	// click on the bottom buttons to change the puzzle image we're working with
-	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+
+		// click on the bottom buttons to change the puzzle images and reset pieces
+		puzzleButtons.forEach(button => {
+		button.addEventListener('click', changeImageSet);
+		button.addEventListener('click', resetPuzzlePieces);
+	});
+
 	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 
-	for (let zone of dropZones) {
+		dropZones.forEach(zone => {
 		zone.addEventListener('dragover', allowDragOver);
 		zone.addEventListener('drop', allowDrop);
-	}
+	});
 
-	// research call, apply and bind
-	changeImageSet.call(puzzleButtons[0]); // emulates a click on the first bottom button
+	// call the function and pass in the first nav button as a reference
+	changeImageSet.call(puzzleButtons[0]);
 })();
